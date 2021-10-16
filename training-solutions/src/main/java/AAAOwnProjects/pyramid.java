74a6f52a9f4df8d0851beb3e2be2d5c3837f4656
k-tam-8 <fluff8@gmail.com>
2021-10-16 22:42:04 +0200

    piramis gyakorló feladat

---
+++ training-solutions/src/main/java/AAAOwnProjects/pyramid.java
@@ -0,0 +1,27 @@
+package AAAOwnProjects;
+
+import java.util.Scanner;
+
+public class pyramid {
+    public static void main(String[] args) {
+        Scanner scanIn= new Scanner(System.in);
+        System.out.print("Piramis magassága: ");
+        int PyramidHeight = scanIn.nextInt();
+
+        for (int i=1; i<=PyramidHeight; i++) {
+
+            for (int k = PyramidHeight-i; k>0; k--) {
+                System.out.print(" ");
+            }
+
+            for (int f=1; f<=i+(i-1); f++) {
+
+                System.out.print("*");
+            }
+                System.out.println();
+            }
+        }
+
+
+    }
+
