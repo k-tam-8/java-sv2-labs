bf1f45775f19abcee0d57a2d2a81c86f2cc2bede
k-tam-8 <fluff8@gmail.com>
2021-10-18 12:36:45 +0200

    Java API lecke feladat

---
+++ training-solutions/src/main/java/javaapi/Upper.java
@@ -0,0 +1,10 @@
+package javaapi;
+
+import java.util.Locale;
+
+public class Upper {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s.toUpperCase());
+    }
+}
