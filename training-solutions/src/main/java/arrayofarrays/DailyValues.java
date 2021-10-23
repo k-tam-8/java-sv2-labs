c8820d24e46ae93cd4ca1f2e0c30b32de935bc67
k-tam-8 <fluff8@gmail.com>
2021-10-23 09:38:49 +0200

    DailyValues osztály elegánsabb megoldása

--- training-solutions/src/main/java/arrayofarrays/DailyValues.java
+++ training-solutions/src/main/java/arrayofarrays/DailyValues.java
@@ -3,7 +3,7 @@ package arrayofarrays;
 public class DailyValues {
     public int[][] getValues() {
         int[][] dailyArr = new int[12][];
-        dailyArr[0]=new int [31];
+/*        dailyArr[0]=new int [31];
         dailyArr[1]=new int [28];
         dailyArr[2]=new int [31];
         dailyArr[3]=new int [30];
@@ -14,7 +14,10 @@ public class DailyValues {
         dailyArr[8]=new int [30];
         dailyArr[9]=new int [31];
         dailyArr[10]=new int [30];
-        dailyArr[11]=new int [31];
+        dailyArr[11]=new int [31];*/
+        int [] months = {31,28,31,30,31,30,31,31,30,31,30,31};
+        for (int i=0; i< months.length;i++)
+            dailyArr[i]=new int [months[i]];
         return dailyArr;
     }
 
