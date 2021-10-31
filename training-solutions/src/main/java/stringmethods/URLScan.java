ba8945406cf376172133f608d3a9467ab85bc3f3
k-tam-8 <fluff8@gmail.com>
2021-10-31 15:06:32 +0100

    módosítás

--- training-solutions/src/main/java/stringmethods/URLScan.java
+++ training-solutions/src/main/java/stringmethods/URLScan.java
@@ -11,10 +11,12 @@ public class URLScan {
         this.protocol = convertProtocol(url);
         this.port = convertPort(url);
         this.host = convertHost(url);
+        this.path = convertPath(url);
+        this.query = convertQuery(url);
     }
 
     public String convertProtocol(String url) {
-        return url.substring(0, url.indexOf(":"));
+        return url.substring(0, url.indexOf(":")).toLowerCase();
     }
 
     public int convertPort(String url) {
@@ -28,13 +30,51 @@ public class URLScan {
                 }
             }
         }
-        return Integer.parseInt(portString);
+        if (portString != "") {
+            return Integer.parseInt(portString);
+        } else return 0;
     }
 
     public String convertHost(String url) {
-        String hostString = "";
-        hostString=url.substring(url.indexOf("://")+3,url.indexOf(url.indexOf("/",url.indexOf("://")+3)));
-        return hostString;
+        int startIndex = url.indexOf("://") + 3;
+        int endIndex = url.indexOf(":", startIndex);
+        if (endIndex < 0) {
+            endIndex = url.indexOf("/", startIndex);
+        }
+        if (endIndex < 0) {
+            endIndex = url.length();
+        }
+        String s = url.substring(startIndex, endIndex);
+        return s.toLowerCase();
+    }
+
+    public String convertPath(String url) {
+        String s = "";
+        int startIndex = url.indexOf("/", 9);
+        int endIndex = url.indexOf("?", startIndex);
+        if (endIndex < 0) {
+            endIndex = url.length();
+        }
+        if (startIndex > 0) {
+            s = url.substring(startIndex + 1, endIndex);
+        }
+        return s;
+    }
+
+    public String convertQuery(String url) {
+        String s = "";
+        int startIndex=url.indexOf("?");
+        if (startIndex>0){
+            s+=url.substring(startIndex+1);
+        }
+        return s;
+    }
+
+    public boolean hasProperty(String key) {
+        return false; //ez mire van?
+    }
+    public String getProperty(String key) {
+        return "";
     }
 
     public String getProtocol() {
@@ -54,10 +94,12 @@ public class URLScan {
     }
 
     public static void main(String[] args) {
-        URLScan urlScan = new URLScan("https://earthquake.usgs.gov:2560/fdsnws/event/1" +
+         URLScan urlScan = new URLScan("https://earthquake.usgs.gov:51265/fdsnws/event/1" +
                 "/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02");
-        System.out.println(urlScan.getProtocol());
-        System.out.println(urlScan.getPort());
+        //URLScan urlScan = new URLScan("http://www.sony.com/");
+        System.out.println("protokoll: " + urlScan.getProtocol());
+        System.out.println("host: " + urlScan.getHost());
+        System.out.println("port: " + urlScan.getPort());
+        System.out.println("path: " + urlScan.getPath());
     }
-    /////////nincs kész
 }
