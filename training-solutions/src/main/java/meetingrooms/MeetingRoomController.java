ae2b3de36901ea82268fef52b35126a08eab3834
k-tam-8 <fluff8@gmail.com>
2021-11-14 19:45:19 +0100

    projektfeladat final-final

--- training-solutions/src/main/java/meetingrooms/MeetingRoomController.java
+++ training-solutions/src/main/java/meetingrooms/MeetingRoomController.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class MeetingRoomController {
     private Office office = new Office();
-    private boolean quitCheck = false;
 
     public void readOffice() {
         Scanner sc = new Scanner(System.in);
@@ -12,10 +11,8 @@ public class MeetingRoomController {
         String roomName = sc.nextLine();
         System.out.print("Kérem a tárgyaló szélességét méterben: ");
         int roomWidth = sc.nextInt();
-        //sc.nextLine();
         System.out.print("Kérem a tárgyaló hosszát méterben: ");
         int roomLength = sc.nextInt();
-        //sc.nextLine();
         MeetingRoom meetingRoom = new MeetingRoom(roomName, roomLength, roomWidth);
         office.addMeetingRoom(meetingRoom);
     }
@@ -34,51 +31,52 @@ public class MeetingRoomController {
     }
 
     public void runMenu() {
-        printMenu();
-        Scanner sc = new Scanner(System.in);
-        int menuChoose = sc.nextInt();
-        sc.nextLine();
-        switch (menuChoose) {
-            case 1:
-                readOffice();
-                break;
-            case 2:
-                office.printNames();
-                break;
-            case 3:
-                office.printNamesReverse();
-                break;
-            case 4:
-                office.printEvenNames();
-                break;
-            case 5:
-                office.printAreas();
-                break;
-            case 6:
-                System.out.print("Keresendő név: ");
-                String name = sc.nextLine();
-                office.printMeetingRoomsWithNames(name);
-                break;
-            case 7:
-                System.out.print("Keresendő névtöredék: ");
-                String nameCont = sc.nextLine();
-                office.printMeetingRoomsContains(nameCont);
-                break;
-            case 8:
-                System.out.print("Tárgyalók amiknek a területük nagyobb mint: ");
-                int area = sc.nextInt();
-                //sc.nextLine();
-                office.printAreasLargerThan(area);
-                break;
-            case 9: quitCheck=true;
+        boolean quitCheck = false;
+        do {
+            printMenu();
+            Scanner sc = new Scanner(System.in);
+            int menuChoose = sc.nextInt();
+            sc.nextLine();
+            switch (menuChoose) {
+                case 1:
+                    readOffice();
+                    break;
+                case 2:
+                    office.printNames();
+                    break;
+                case 3:
+                    office.printNamesReverse();
+                    break;
+                case 4:
+                    office.printEvenNames();
+                    break;
+                case 5:
+                    office.printAreas();
+                    break;
+                case 6:
+                    System.out.print("Keresendő név: ");
+                    String name = sc.nextLine();
+                    office.printMeetingRoomsWithNames(name);
+                    break;
+                case 7:
+                    System.out.print("Keresendő névtöredék: ");
+                    String nameCont = sc.nextLine();
+                    office.printMeetingRoomsContains(nameCont);
+                    break;
+                case 8:
+                    System.out.print("Tárgyalók amiknek a területük nagyobb mint: ");
+                    int area = sc.nextInt();
+                    office.printAreasLargerThan(area);
+                    break;
+                case 9:
+                    quitCheck = true;
+            }
         }
+        while (!quitCheck);
     }
 
     public static void main(String[] args) {
         MeetingRoomController meetingRoomController = new MeetingRoomController();
-        do {
-            meetingRoomController.runMenu();
-        }
-        while (!meetingRoomController.quitCheck);
+        meetingRoomController.runMenu();
     }
 }
